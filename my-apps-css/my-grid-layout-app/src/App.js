387b9bc5f98f352a7f93c2ffbfc9e2a1ab85ea16
Ambili Cheran <ambili.cheran@gmail.com>
2021-10-01 20:15:26 +0200

    com1

---
+++ my-apps-css/my-grid-layout-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <article>
+      <div class="grid-container">
+        <div class="item1">Header</div>
+        <div class="item2">Menu</div>
+        <div class="item3">Main</div>
+        <div class="item4">Right</div>
+        <div class="item5">Footer</div>
+      </div>
+    </article>
+  );
+}
+
+export default App;
